9cfeafc5ba38ec3b2763d6e53c220f95f10e4d50
顾浩鑫 <guhaoxin520@pingan.com.cn>
2018-03-02 16:44:26 +0800

    [C][C:顾浩鑫]选择文件夹后，即需要马上把改文件夹的图像展示出来，否则页面很难看

--- js/directorySelect.js
+++ js/directorySelect.js
@@ -5,10 +5,12 @@ function directorySelect(e) {
   console.log(files);
   pathList = [];
   fileList = [];
+  var dcmFileList = [];
   for (const key in files) {
     if (files.hasOwnProperty(key)) {
       var val = files[key];
       if(val.name.match(/\.dcm/)) {
+        dcmFileList.push(val);
         const { webkitRelativePath } = val;
         const pathArr = webkitRelativePath.split('\/');
         const relativePath = pathArr[pathArr.length - 2];
@@ -19,9 +21,11 @@ function directorySelect(e) {
       }
     }
   }
-  console.log(pathList)
-  console.log(fileList)
+  // console.log(pathList)
+  // console.log(fileList)
+  // console.log(dcmFileList);
   showDir(pathList);
+  showImages(dcmFileList);
 }
 
 function showDir(list) {
@@ -36,6 +40,28 @@ function showDir(list) {
   const dirDom = document.getElementById('jsdir');
   dirDom.innerHTML = html;
 }
+
+function showImages(dcmFiles) {
+  var dicomImage = new DICOMImage();
+  var SeriesSets = {};
+  dicomImage.loadDicomFiles(dcmFiles).then(function (seriesSets) {
+    SeriesSets = seriesSets;
+  }).then(function (res) {
+    var domCanvas = document.getElementById("dicomImage");
+    // uploaderprogress.innerHTML = '100%';
+
+    var dicomViewer = new DICOMViewer(domCanvas);
+    var seriesID = Object.keys(SeriesSets)[0];
+    // console(seriesID);
+    // 查看器点击事件 
+    bindEvent(dicomViewer, SeriesSets[seriesID].length);
+
+    var pointsSet = [];
+    dicomViewer.setDcmSeriesInfo(SeriesSets[seriesID], pointsSet);
+    filesDicom(SeriesSets, dicomViewer);
+  })
+}
+
 function test(e) {
   console.log();
   const relativePath = e.innerText;
